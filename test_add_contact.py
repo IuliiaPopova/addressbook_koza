0d7607b96465628e0fec035e16e859c757283f24
IuliiaPopova <popovasqa@gmail.com>
2020-05-20 10:00:55 -0700

    fixure1

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,51 +1,17 @@
-import unittest
+import pytest
 
-from selenium import webdriver
-
+from application import Application
 from contact import Contact
 
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        # self.driver.set_window_position(0, -1000)
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(3)
-
-    def test_add_contact(self):
-        self.login(username="admin", password="secret")
-        self.create_new_cont(Contact(first_name="Olga", last_name="Kozlova"))
-        self.logout()
-
-    def open_home_page(self):
-        self.driver.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        self.open_home_page()
-        self.driver.find_element_by_name("user").send_keys(username)
-        self.driver.find_element_by_name("pass").send_keys(password)
-        self.driver.find_element_by_xpath('//input[@type = "submit"]').click()
-
-    def add_new_cont(self):
-        self.driver.find_element_by_xpath('//a[@href  = "edit.php"]').click()
-
-    def add_address(self):
-        self.driver.find_element_by_xpath('//input[@name = "quickadd"][1]').click()
-
-    def create_new_cont(self, contact):
-        self.add_new_cont()
-        self.add_address()
-        self.driver.find_element_by_name("firstname").send_keys(contact.first_name)
-        self.driver.find_element_by_name("lastname").send_keys(contact.last_name)
-        self.driver.find_element_by_name("submit").click()
-
-    #
-    def logout(self):
-        self.driver.find_element_by_link_text("Logout").click()
-
-    def tearDown(self):
-        self.driver.quit()
+@pytest.fixture
+def app(request):
+    application = Application()
+    request.addfinalizer(application.tear_down)
+    return application
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_new_cont(Contact(first_name="Olga", last_name="Kozlova"))
+    app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,71 +1,24 @@
-# -*- coding: utf-8 -*-
-import unittest
+import pytest
 
-from selenium import webdriver
-
+from application import Application
 from group import Group
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        #self.driver.set_window_position(0, -1000)
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(3)
-
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        group = Group(name="dfgdfg", header="dfgdfg", footer="dfgfghgfhg")
-        self.create_group(group)
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def open_home_page(self):
-        self.driver.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        self.open_home_page()
-        self.driver.find_element_by_name("user").click()
-        self.driver.find_element_by_name("user").clear()
-        self.driver.find_element_by_name("user").send_keys(username)
-        self.driver.find_element_by_name("pass").click()
-        self.driver.find_element_by_name("pass").clear()
-        self.driver.find_element_by_name("pass").send_keys(password)
-        self.driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def open_group_page(self):
-        self.driver.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        self.open_group_page()
-        self.driver.find_element_by_name("new").click()
-        self.driver.find_element_by_name("group_name").click()
-        self.driver.find_element_by_name("group_name").clear()
-        self.driver.find_element_by_name("group_name").send_keys(group.name)
-        self.driver.find_element_by_name("group_header").click()
-        self.driver.find_element_by_name("group_header").clear()
-        self.driver.find_element_by_name("group_header").send_keys(group.header)
-        self.driver.find_element_by_name("group_footer").click()
-        self.driver.find_element_by_name("group_footer").clear()
-        self.driver.find_element_by_name("group_footer").send_keys(group.footer)
-        self.driver.find_element_by_name("submit").click()
-        self.return_to_group_page()
-
-    def return_to_group_page(self):
-        # return to group page
-        self.driver.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        # logout
-        self.driver.find_element_by_link_text("Logout").click()
-
-    def tearDown(self):
-        self.driver.quit()
+@pytest.fixture
+def app(request):
+    application = Application()
+    request.addfinalizer(application.tear_down)
+    return application
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    group = Group(name="dfgdfg", header="dfgdfg", footer="dfgfghgfhg")
+    app.create_group(group)
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
